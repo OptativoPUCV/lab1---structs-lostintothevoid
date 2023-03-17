57d505c4cfd37a15e7ecf38c8a6deac062f0217d
Replit user <>
2023-03-17 11:42:13 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -14,7 +14,7 @@ intercambia los valores de las variables apuntadas por a y b.
 */
 void swap(int *a, int *b) {
   int basura;
-  if (*a <> *b){
+  if (*a ≠ *b){
     basura = *a;
     *a = *b;
     *b = *a;
